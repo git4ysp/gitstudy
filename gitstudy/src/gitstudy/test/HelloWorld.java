19ac24984ba000d462d8c5fa1ec0bf41bce198bb
git4ysp <genpys@outlook.com>
2015-05-23 17:55:17 +0800

    add

---
+++ gitstudy/src/gitstudy/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package gitstudy.test;
+
+/**
+ * 
+ * @author shipeng.yu
+ * @date 2015年5月23日下午5:54:25
+ * @description
+ * @version V1.0
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
---
+++ gitstudy/src/gitstudy/test/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author shipeng.yu
+ * @date 2015年5月23日下午5:54:11
+ * @description
+ * @version V1.0
+ */
+/**
+ * @author ysp
+ *
+ */
+package gitstudy.test;
